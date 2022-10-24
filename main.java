6587772fd935d1c63a160b79240d3f8858082e3d
LuthfiND <92884635+LuthfiND@users.noreply.github.com>
2022-10-24 20:13:53 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main{
+    public static void main(String[] args) {
+    for(int i = 1; i<=10; i++)
+        if(i%2 == 0){
+            System.out.println("angka" + i + "ini adalah genap");
+        }else{
+            System.out.println("angka" + i + "ini adalah ganjil");
+        }
+    }
+}
